60dc0721fc6d8685a60c356f8a961666d1d585b0
ilmir22 <152898248+ilmir22@users.noreply.github.com>
2024-01-31 20:39:56 +0300

    Add files via upload
    Работа с сервером, обращение

---
+++ practick2.js
@@ -0,0 +1,39 @@
+// let age = 18;
+// userAccessStatus = age > 18 ? "Доступ разрешен":
+// age == 18 ? "Доступ разрешен. Вам 18 лет!":
+// "Доступ запрещен Вам меньше 18 лет!"
+// console.log(userAccessStatus)
+//-----------------------------------------------------
+// let dayofweek = "Monday";
+
+// switch(dayofweek){
+//     case "Sunday": //case - Ищет который вариант правильный, нужный
+//         console.log("Сегодня воскресенье");
+//         break
+//     case "Monday":
+//         console.log("Сегодня понедельник");
+//         break //Закрывающий оператор
+//     case "Tuesday":
+//         console.log("Сегодня вторник");
+//         break    
+// }
+// --------------------------------------------
+// for (let i = 0; i<5; i++){
+//     console.log(i)
+// }
+// -------------------------------------------------
+// const my_arr = ['a', 'b', 'c', 'd',]
+// for(let i = 0; i<my_arr.length; i++){
+//     console.log(my_arr[i]);
+// }
+//------------------------------------------------------
+// let x = 5;
+// while (x < 5) {
+//     console.log(x);
+//     x++;
+// }
+// let y = 5;
+// do {
+//     console.log(y)
+//     y++
+// }while(y<5)
